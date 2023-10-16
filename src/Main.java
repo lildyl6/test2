1bea149e1e10a49f6c3202e35fb9a1881587cd08
dylanjones <4dylan.jone@gmail.com>
2023-10-16 13:00:20 -0400

    webhook test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Its pushed");
-        System.out.println("Webhook3");
+        System.out.println("Webhook test");
     }
 }
