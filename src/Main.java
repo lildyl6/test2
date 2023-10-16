a2bcc1791038a93c849fb2322e4f0349c25d094d
dylanjones <4dylan.jone@gmail.com>
2023-10-16 13:26:00 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Webhook test");
+        System.out.println("Yay");
+        System.out.println(fibonacci(7));
+    }
+
+    public static int fibonacci(int x){
+        if(x <= 1){
+            return 0;
+        } else if (x <= 3){
+            return 1;
+        } else{
+            return fibonacci(x - 1) + fibonacci(x - 2);
+        }
     }
 }
